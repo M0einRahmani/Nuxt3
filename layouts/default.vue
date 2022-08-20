335d9fb78bccd77acf5f090a7c6183896c39e13d
Moein Rahmani <moeinrahmani951@gmail.com>
2022-08-20 15:39:15 +0430

    before tailwind 3

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Title>صفحه اصلی</Title>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/">خانه</NuxtLink></li>
+        <li><NuxtLink to="/about">درباره ی ما</NuxtLink></li>
+        <li><NuxtLink to="/posts">پست ها</NuxtLink></li>
+      </ul>
+    </nav>
+    <slot />
+  </div>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+  <Title>درباره ی ما</Title>
+  This is about page
+  <img src="Wallpaper - NuxtJS Crash Course.jpg" alt="image">
+</div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout : "custom",
+})
+</script>
